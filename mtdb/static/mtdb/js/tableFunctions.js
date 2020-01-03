88c3949420d16bfcc88c072b9be9568f618e3d8a
unknown <yosuke.seki@gmail.com>
2020-01-02 19:25:37 -0600

    1.2.20

--- mtdb/static/mtdb/js/tableFunctions.js
+++ mtdb/static/mtdb/js/tableFunctions.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         $("#indexTable tr td:contains('No Ratings')").each(function (index, element) {
             $(this).parent("tr").toggleClass("hide");
         });
-        reapplyStripes()
+        reapplyStripes();
     });
 
     $("#filterRow").keyup(function () {
@@ -21,30 +21,35 @@ $(document).ready(function () {
                 return not_found;
             });
         });
-        reapplyStripes()
+        reapplyStripes();
     });
 
+    var icon;
     $('th').click(function () {
-        var icon = $(this).find('span.sort-icon')
+        icon = $(this).find('span.sort-icon')
         icon.html('<i class="fas fa-sort-up"></i>')
         var table = $(this).parents('table').eq(0)
-        var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
         this.asc = !this.asc
-        if (!this.asc) {
-            rows = rows.reverse()
-            icon.html('<i class="fas fa-sort-down"></i>')
-        }
+        var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index(), !this.asc))
         for (var i = 0; i < rows.length; i++) {
             table.append(rows[i])
         }
         reapplyStripes()
     })
-    function comparer(index) {
+    function comparer(index, desc) {
         return function (a, b) {
-            var valA = getCellValue(a, index), valB = getCellValue(b, index)
-            return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+            var valA = getCellValue(a, index),
+                valB = getCellValue(b, index)
+            var result = $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB);
+            // If desc is set we sort descending otherwise ascending by default.
+            if (desc) {
+                result *= -1;
+                icon.html('<i class="fas fa-sort-down"></i>')
+            }
+            return result;
         }
     }
+    // function getCellValue(row, index) { return $(row).children('td').eq(index).text() }
     function getCellValue(row, index) {
         var cellValue = $(row).children('td').eq(index).text().trim()
         if (cellValue === "No Ratings") {
