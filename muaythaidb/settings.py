863f8aa10019b98fde173f86ccd900827638ae33
ysmt1 <yosuke.seki@gmail.com>
2020-12-28 15:12:15 -0600

    disabled heroku ci testrunner

--- muaythaidb/settings.py
+++ muaythaidb/settings.py
@@ -149,4 +149,4 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), test_runner=False)
